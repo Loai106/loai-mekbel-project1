98cf310717a1f57751b9a3d6675d09dabdb95c09
Loai <43700105+Loai106@users.noreply.github.com>
2024-09-28 22:37:23 +0300

    uploading project's files

---
+++ index.js
@@ -0,0 +1,18 @@
+const cards_container = document.querySelector('.cards_container');
+
+fetch('./topics.json').then(res =>res.json())
+.then(data =>{
+    data.forEach(topic => {
+            cards_container.insertAdjacentHTML('afterbegin',`<div class="card">
+                    <img src="assets/logos/${topic.image}"/>
+                    <div class="content">
+                        <div class="card_details">
+                            <p>${topic.category}</p>
+                            <h3>${topic.topic}</h3>
+                        </div>
+                        <div>⭐⭐⭐⭐</div>
+                        <p>Authour: ${topic.name}</p>
+                    </div>
+        </div>`)
+    });
+} );
